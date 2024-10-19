920897a5476199cb607ffbd0ced0bf8440b386de
zaringleb <zaringleb@gmail.com>
2024-10-19 15:27:30 +0100

    Create submission.py

---
+++ submission.py
@@ -0,0 +1,29 @@
+import math
+from collections import deque
+from itertools import chain
+from typing import Callable
+
+import einops
+import numpy as np
+import torch
+import torch.nn.functional as F  # noqa: N812
+import torchvision
+from huggingface_hub import PyTorchModelHubMixin
+from torch import Tensor, nn
+from torchvision.models._utils import IntermediateLayerGetter
+from torchvision.ops.misc import FrozenBatchNorm2d
+
+from lerobot.common.policies.act.configuration_act import ACTConfig
+from lerobot.common.policies.normalize import Normalize, Unnormalize
+
+
+class DummyACTPolicy(
+    nn.Module,
+    PyTorchModelHubMixin,
+    library_name="lerobot",
+    repo_url="https://github.com/huggingface/lerobot",
+    tags=["robotics", "act"],
+):
+  def __init__(self):
+    super().__init__()
+    print("I'm just a dummy script, don't ask much from me.")
